594e2df8e4e1d2c3647fe7b98b7dd51623f1c69e
Sreeja Yathipathi <s2433012@kpuenta.ca>
2025-07-28 19:18:13 -0700

    Added some sounds

--- 1333_Template/Assets/1333_RTS/StudentWork/Scripts/Enemy/EnemyBaseLoader.cs
+++ 1333_Template/Assets/1333_RTS/StudentWork/Scripts/Enemy/EnemyBaseLoader.cs
@@ -8,10 +8,11 @@ public class EnemyBaseLoader : MonoBehaviour
 
     private void Start()
     {
-        foreach (var building in _layout.buildings)
+        /*foreach (var building in _layout.buildings)
         {
             // Use world position directly
-            Vector3 spawnPos = building.position;
+            //Vector3 spawnPos = building.position;
+            Vector3 spawnPos = _gridManager.ClampWorldToGrid(building.position);
 
             GameObject placed = Instantiate(
                 building.prefab,
@@ -37,7 +38,41 @@ public class EnemyBaseLoader : MonoBehaviour
                     }
                 }
             }
+        }*/
+
+        foreach (var building in _layout.buildings)
+        {
+            // Snap building position to grid
+            Vector3 spawnPos = _gridManager.ClampWorldToGrid(building.position);
+
+            // Instantiate
+            GameObject placed = Instantiate(
+                building.prefab,
+                spawnPos,
+                Quaternion.Euler(-90f, building.rotationY, 0f)
+            );
+            placed.name = $"[Enemy] {building.prefab.name}";
+
+            // Get footprint size from BuildingItemData
+            Vector2Int size = Vector2Int.one;
+            BuildingItemReference itemRef = building.prefab.GetComponent<BuildingItemReference>();
+            if (itemRef != null)
+                size = itemRef.Data.footprintSize;
+
+            // Mark grid nodes occupied only for footprint
+            Vector3 basePos = spawnPos - GetFootprintOffset(size);
+            for (int dx = 0; dx < size.x; dx++)
+            {
+                for (int dy = 0; dy < size.y; dy++)
+                {
+                    Vector3 pos = basePos + new Vector3(dx, 0, dy);
+                    GridNode node = _gridManager.GetNodeFromWorldPosition(pos);
+                    if (node != null)
+                        node.IsOccupied = true;
+                }
+            }
         }
+
 
         Debug.Log("[EnemyBaseLoader] Enemy base loaded.");
     }
